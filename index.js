c491fd3a1f1129f002c64102966701c5cc611489
EcheCostabel <exequielcostabel@gmail.com>
2023-04-03 09:33:25 -0300

    first route

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const express = require("express");
 
 
-console.log('hola m')
+
+//Creo el servidor de express
+const app = express();
+
+
+
+//Rutas 
+
+app.get('/', (req, res) => {
+
+    console.log('/ Requerido');
+    res.json({
+        ok: true,
+        
+    })
+});
+
+
+
+
+//Escucha al puerto 3001
+app.listen(3001, () => {
+    console.log(`Servidor corriendo en puerto ${3001}`)
+})
