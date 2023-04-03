429db67ebee7ba73a781e07e14ebace4663f0c41
EcheCostabel <exequielcostabel@gmail.com>
2023-04-03 09:44:48 -0300

    .env and public folder

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require("express");
+require('dotenv').config();
 
-
+console.log(process.env)
 
 //Creo el servidor de express
 const app = express();
 
 
+//Directorio  Publico
+app.use(express.static('public'));
+
+
 
 //Rutas 
+// app.get('/', (req, res) => {
 
-app.get('/', (req, res) => {
+//     console.log('/ Requerido');
+//     res.json({
+//         ok: true,
 
-    console.log('/ Requerido');
-    res.json({
-        ok: true,
-        
-    })
-});
+//     })
+// });
 
 
 
 
 //Escucha al puerto 3001
-app.listen(3001, () => {
-    console.log(`Servidor corriendo en puerto ${3001}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
