fce8d87dc01bae67f013789e49550981f222345c
EcheCostabel <exequielcostabel@gmail.com>
2023-04-03 15:46:43 -0300

    user validate

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,27 @@ const User = require('../models/User')
 
 const createUser = async(req, res) => {
 
-    // const {name, email, password} = req.body;
+    const { email, password} = req.body;
 
     try {
-        const user = new User(req.body); //creo un nuevo usuario con lo que me llega en req.body
+        let user = await User.findOne({ email }); 
+        console.log(user)
+
+        if ( user ) { //si ya existe un usuario con el mismo email retorno esto
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario ya existe con ese correo'
+            })
+        }
+         user = new User(req.body); //creo un nuevo usuario con lo que me llega en req.body
     
         await user.save();  // lo guardo en la base de datos de mongo
        
     
         res.status(201).json({
           ok: true,
-          msg: 'registro',
+          uid: user.id,
+          name: user.name
     
     });
     } catch (error) {
