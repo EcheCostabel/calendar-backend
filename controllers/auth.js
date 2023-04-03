667798c52db9374287b03e3d8db5df5d00b7bfcf
EcheCostabel <exequielcostabel@gmail.com>
2023-04-03 10:50:20 -0300

    catch data from post

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,48 @@
 
 
+
+
 const createUser = (req, res) => {
+
+    const {name, email, password} = req.body;
+
+    if( name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre no puede ser menor a 5 letras'
+        })
+    }
+
     res.json({
-        ok: true,
-        msg: 'registro'
-    })
-};
+      ok: true,
+      msg: 'create',
+      name,
+      email,
+      password
+});
+}
+
+
+
 
 const loginUser = (req, res) => {
+
+    const { email, password} = req.body;
+
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     })
 };
 
+
+
+
 const validateToken = (req, res) => {
+
+
     res.json({
         ok: true,
         msg: 'validateToken'
