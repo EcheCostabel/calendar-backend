b640d12cc02d5cddad09f62d729003917c3b49cc
EcheCostabel <exequielcostabel@gmail.com>
2023-04-03 19:22:43 -0300

    validate required fields

--- routes/events.js
+++ routes/events.js
@@ -7,18 +7,27 @@ const {Router} = require('express');
 const router = Router();
 const { jwtValidator } = require('../middlewares/jwt-validator');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
-
+const { check } = require('express-validator');
+const { imputsValidator } = require('../middlewares/imputs-validator');
+const { isDate } = require('../helpers/isDate');
 
 router.use(jwtValidator);   //Esto es porque todas las peticiones tienen que pasar por la validacion del jwtValidator
 
 
 // Obtener eventos
-router.get('/',  getEvents);
+router.get('/',getEvents);
 
 
 
 //Crear nuevo evento
-router.post('/', createEvent);
+router.post('/', 
+[
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'La fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'La fecha de finalizacion es obligatoria').custom(isDate),
+    imputsValidator
+]
+, createEvent);
 
 
 
