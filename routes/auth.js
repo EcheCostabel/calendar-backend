3816f0c14f8220eb209d00783cb513f017a3f28e
EcheCostabel <exequielcostabel@gmail.com>
2023-04-03 11:44:36 -0300

    custom middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const {Router} = require('express');
 const { check } = require('express-validator')
 const router = Router();
 const { createUser, loginUser, validateToken } = require('../controllers/auth');
+const { imputsValidator } = require('../middlewares/imputs-validator');
 
 
 
@@ -14,6 +15,7 @@ router.post('/new',
     check('name', 'El nombre es obligatorio').not().isEmpty(), // exto es para validar los campos del formulario de creacion
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe ser de 6 caracteres').isLength({min : 6}),
+    imputsValidator
 ], 
  createUser);
 
@@ -22,6 +24,7 @@ router.post('/',
 [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe ser de 6 caracteres').isLength({min : 6}),
+    imputsValidator
 ],
 loginUser);
 
