20a7d8e5a9884539503553093d809cc0157abc09
EcheCostabel <exequielcostabel@gmail.com>
2023-04-04 18:11:45 -0300

    vercel settings

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const { dbConnection } = require('./database/config');
-const cors = require('cors')
-
+const cors = require('cors');
 
 
 //Creo el servidor de express
@@ -14,7 +13,6 @@ dbConnection();
 
 
 // CORS
-
 app.use(cors());
 
 //Directorio  Publico
@@ -34,4 +32,4 @@ app.use('/api/events', require('./routes/events') );
 //Escucha al puerto 3001
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
-})
+});
