de809f71c445c996809c52049df4f91e902f2df3
EcheCostabel <exequielcostabel@gmail.com>
2023-04-04 09:10:08 -0300

    event saved in db

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,4 @@
-
+const Event = require('../models/Event')
 
 const getEvents = (req, res) => {
 
@@ -10,16 +10,29 @@ const getEvents = (req, res) => {
 
 
 
-const createEvent = (req, res) => {
+const createEvent = async(req, res) => {
 
-    
+    const event = new Event(req.body);
 
+    try {
 
+        event.user = req.uid;
 
-    res.json({
+      const savedEvent = await event.save();
+
+       res.json({
         ok: true,
-        msg: 'createEvent'
-    })
+        event : savedEvent
+       })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Comuniquese con el administrador'
+        })
+    }
+
 };
 
 
