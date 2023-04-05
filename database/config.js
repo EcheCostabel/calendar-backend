a7412d40d49a98b0c6897e360c22c97b4dc11d35
EcheCostabel <exequielcostabel@gmail.com>
2023-04-05 18:13:12 -0300

    trying now

--- database/config.js
+++ database/config.js
@@ -5,13 +5,9 @@ const dbConnection = async () => {
 
 
     try {
-       await  mongoose.connect(process.env.DB_CNN1, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-         
-           
-       });
-       console.log('db online')
+       await  mongoose.connect('mongodb+srv://mern_user:0JGwqdEUXej6xuUs@cluster0.7ctbvek.mongodb.net/?retryWrites=true&w=majority');
+       console.log('db online');
+       console.log(DB_CNN1)
 
     } catch (error) {
         console.log(error);
